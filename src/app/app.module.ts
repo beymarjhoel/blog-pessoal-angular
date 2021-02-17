df9cd4793413713ccc50562a50c3fc9fdb1dcff5
beymarjhoel <beymarjhlima@gmail.com>
2021-02-16 22:05:44 -0300

    Criação do menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+//Aqui ficam todos os modulos que vamos utilizar
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
+  //Declaração dos componentes
   declarations: [
-    AppComponent
+    //Componente Padrão
+    AppComponent,
+    //Componentes criados
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
